7ab4cc4e57e44a359b58b14fc28657d5138c929a
BJUTDingLu <1301123219@163.com>
2020-11-10 00:00:38 +0800

    risk prediction of illegal fund raising

---
+++ Risk_Prediction_Of_Illegal_Fund_Raising/code/base_info.py
@@ -0,0 +1,187 @@
+
+from sklearn.preprocessing import LabelEncoder
+import datetime
+import pandas as pd
+
+
+"""
+reccap:实缴资本
+    数值信息，空值用-1值填充
+    
+enttypeminu:企业类型细类
+    种类信息，空值用'-1'填充
+    
+venind:风险行业
+    种类信息，空值用'-1'填充, 也可以删除
+    
+opfrom:经营期限起
+opto:经营期限止
+    时间信息，数值信息，空置用-1填充
+    计算开始year
+    计算结束year
+    计算时间差
+    
+compform:组织形式
+    删除
+
+
+enttypeitem:企业类型小类
+    种类信息，空值用'-1'填充
+    
+empnum:从业人数
+    数值信息，空值用-1值填充，也可以删除
+    
+regcap:注册资本（金）
+    数值信息，空值用-1值填充
+    
+industryco:行业细类代码
+    种类信息，空值用'-1'填充
+    
+oploc:经营场所
+    种类信息，空值用'-1'填充
+
+oplocdistrict:行政区划代码   
+    种类信息，空值用'-1'填充
+    
+regtype:主题登记类型
+    种类信息，空值用'-1'填充
+    
+townsign:是否城镇
+    种类信息，空值用'-1'填充
+    
+adbusign:是否广告经营
+    种类信息，空值用'-1'填充
+    
+jobid:职位标识
+    种类信息，空值用'-1'填充
+    
+orgid:机构标识
+    种类信息，空值用'-1'填充
+    
+state:状态
+    种类信息，空值用'-1'填充
+    
+opform:经营方式
+    种类信息，空值用'-1'填充
+    
+enttype:企业类型
+    种类信息，空值用'-1'填充
+    
+opscope:经营范围
+    包括中文语言特征，暂时直接删除
+
+dom:经营地址
+     种类信息，空值用'-1'填充，种类过多，labelEncoder视为数值类型， 也可以删除
+     
+industryphy:行业类别代码
+      种类信息，空值用'-1'填充
+      
+enttypegb:企业（机构）类型
+    种类信息，空值用'-1'填充
+    
+    
+"""
+
+
+
+
+class BaseInfo:
+    def __init__(self, data, type='train'):
+        self.data = data
+        self.data_type = {
+            'opfrom': 'time',
+            'opto': 'time',
+            'reccap': 'int64',
+            'enttypeminu': 'category',
+            'venind': 'category',
+            'enttypeitem': 'category',
+            'empnum': 'int64',
+            'regcap': 'int64',
+            'industryco': 'category',
+            'oploc': 'category',
+            'oplocdistrict': 'category',
+            'regtype': 'category',
+            'townsign': 'category',
+            'adbusign': 'category',
+            'jobid': 'category',
+            'orgid': 'category',
+            'state': 'category',
+            'enttype': 'category',
+            'dom': 'category',
+            'industryphy': 'category',
+            'enttypegb': 'category',
+            'opform': 'category'
+        }
+        self.useless_columns = [
+            'ptbusscope',
+            'midpreindcode',
+            'protype',
+            'forreccap',
+            'congro',
+            'forregcap',
+            'exenum',
+            'parnum',
+            'compform',
+            'opscope',
+            'id',
+        ]
+
+        if type == 'test':
+            self.useless_columns.remove('id')
+            self.useless_columns.append('score')
+
+        return
+
+    def fill_nan(self, name, value, column_type):
+        self.data[name] = self.data[name].fillna(value)
+        self.data[name] = self.data[name].astype(column_type)
+        return
+
+    def label_encoder(self, name, column_type):
+        label_encode = LabelEncoder()
+        value_data = self.data[self.data[name].isnull() == 0]
+        null_data = self.data[self.data[name].isnull() != 0]
+        value_data[name] = label_encode.fit_transform(value_data[name])
+        self.data = pd.concat([null_data, value_data])
+        self.data[name] = self.data[name].astype(column_type)
+        return
+
+    def drop_columns(self, drop_columns):
+        self.data.drop(drop_columns, axis=1, inplace=True)
+        return
+
+    def unify_time(self, name):
+        value_data = self.data[self.data[name].isnull() == 0]
+        null_data = self.data[self.data[name].isnull() != 0]
+        value_data[name] = value_data[name].apply(
+            lambda x: x if len(x) > 10 else (x + " 00:00:00"))
+        value_data[name] = value_data[name].apply(
+            lambda x: x if x is None else datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+        value_data[name] = value_data[name].apply(lambda x: x.year)
+        self.data = pd.concat([value_data, null_data])
+        return
+
+
+    def process_v1(self):
+        for name in self.data_type.keys():
+            if self.data_type.get(name) == 'category':
+                self.label_encoder(name,'category')
+            elif self.data_type.get(name) == 'int64':
+                mean = self.data[self.data[name].isnull() == 0][name].mean()
+                self.fill_nan(name, mean, 'int64')
+            elif self.data_type.get(name) == 'time':
+                self.unify_time(name)
+                mode = self.data[name].mode()[0]
+                self.fill_nan(name, mode, 'int64')
+
+        self.data['diff_year'] = (self.data['opto'] - self.data['opfrom']).astype('int64')
+        self.drop_columns(self.useless_columns)
+        return self.data
+
+
+
+
+
+
+
+
---
+++ Risk_Prediction_Of_Illegal_Fund_Raising/code/tools.py
@@ -0,0 +1,211 @@
+# -*- coding:utf-8 -*-
+"""
+Tools for Risk Prediction Of Illegal Fund Raising
+
+Description:
+this file for tools with basic functions.
+
+"""
+# 2020/11/07,Junlu,Ding,create
+from sklearn.preprocessing import LabelEncoder
+
+import datetime
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from bubbly.bubbly import bubbleplot
+from plotly.offline import iplot
+from  sklearn.model_selection import train_test_split
+
+
+
+def drop_miss_data(data_frame, miss_threshold):
+    """
+
+    :param data_frame:
+    :param miss_threshold:
+    :return:
+    """
+
+    miss_rate = (data_frame.isnull().sum() / len(data_frame)).to_frame().reset_index()
+    miss_rate.columns = ['name', 'miss_rate']
+    miss_rate = miss_rate.sort_values(by='miss_rate', ascending=False)
+    drop_columns = list(miss_rate[miss_rate['miss_rate'] > miss_threshold]['name'])
+    print("the drop columns is:")
+    print(drop_columns)
+    data_frame.drop(drop_columns, inplace=True, axis=1)
+    return miss_rate, data_frame
+
+
+def category_number_info(data_frame):
+    """
+
+    :param data_frame:
+    :return:
+    """
+    names = []
+    is_nan = []
+    is_repeat = []
+    unique_num = []
+
+    data_size = len(data_frame)
+    for name in data_frame.columns:
+        names.append(name)
+        unique_num.append(len(data_frame[name].unique()))
+        if data_frame[[name]].isnull().sum()[0] > 0:
+            is_nan.append('YES')
+        else:
+            is_nan.append('NO')
+        if len(data_frame[name].unique()) == data_size:
+            is_repeat.append('NO')
+        else:
+            is_repeat.append('YES')
+
+    number_info = pd.DataFrame({
+        'name': names,
+        'is_nan': is_nan,
+        'is_repeat': is_repeat,
+        'unique_num': unique_num
+    })
+    return number_info
+
+
+def label_encoder(data_frame, column_names):
+    """
+
+    :param data_frame:
+    :param column_names:
+    :return:
+    """
+    label_encode = LabelEncoder()
+    for name in column_names:
+        value_data = data_frame[data_frame[name].isnull() == 0]
+        null_data = data_frame[data_frame[name].isnull() != 0]
+        value_data[name] = label_encode.fit_transform(value_data[name])
+        data_frame = pd.concat([null_data, value_data])
+    return data_frame
+
+
+def fill_nan(data_frame, column_name, fill_way):
+    """
+
+    :param data_frame:
+    :param column_name:
+    :param fill_way:
+    :return:
+    """
+    if fill_way == "mean":
+        mean = data_frame[column_name].mean()
+        data_frame[column_name] =data_frame[column_name].fillna(mean)
+    elif fill_way == "mode":
+        mode = data_frame[column_name].mode()[0]
+        data_frame[column_name] = data_frame[column_name].fillna(mode)
+    elif fill_way == "max":
+        max = data_frame[column_name].max()[0]
+        data_frame[column_name] = data_frame[column_name].fillna(max)
+    elif fill_way == "min":
+        min = data_frame[column_name].min()[0]
+        data_frame[column_name] = data_frame[column_name].fillna(min)
+    elif fill_way == 'median':
+        median = data_frame[column_name].median()
+        data_frame[column_name] = data_frame[column_name].fillna(median)
+    return data_frame
+
+def predict_result(model, test_data, result_path):
+    """
+
+    :param model:
+    :param test_data:
+    :param result_path:
+    :return:
+    """
+
+    test_id = test_data['id']
+    test_data.drop(['id'], inplace=True, axis=1)
+    test_prob = model.predict_proba(test_data)[:, 1]
+    result = pd.DataFrame({'id': test_id, 'score': test_prob})
+    # result = result.groupby("id").agg('mean').reset_index()
+    result.to_csv(result_path, index=False)
+    return
+
+def get_balance_data(data_frame):
+    """
+
+    :param data_frame:
+    :return:
+    """
+    pos_data = data_frame[data_frame['label'] == 1]
+    neg_data = data_frame[data_frame['label'] == 0]
+    neg_data = neg_data.sample(n=len(pos_data), axis=0, random_state=2020, replace=True)
+    data_frame = pd.concat([neg_data, pos_data])
+    return data_frame
+
+
+def plot_hist(data_frame,feature_name,label_name):
+    fg = sns.FacetGrid(data_frame, col=label_name)
+    fg.map(plt.hist, feature_name)
+    plt.show()
+    return
+
+def plot_bubble(data_frame, x_name, y_name, z_name, color_name, size_name):
+
+    data_frame['id'] = list(range(len(data_frame)))
+    figure = bubbleplot(dataset=data_frame,
+                        x_column=x_name,
+                        y_column=y_name,
+                        z_column=z_name,
+                        bubble_column='id',
+                        size_column=size_name,
+                        color_column=color_name,
+                        x_title=x_name,
+                        y_title=y_name,
+                        z_title=z_name,
+                        title='bubble',
+                        x_logscale=False,
+                        scale_bubble=0.1,
+                        height=600)
+    iplot(figure, config={'scrollzoom': True})
+    plt.show()
+    data_frame.drop(['id'], axis=1, inplace=True)
+    return
+
+def plot_kde(data_frame, feature_name, label_name):
+    fg = sns.FacetGrid(data_frame, hue=label_name, size=10)
+    fg.map(sns.kdeplot, feature_name)
+    fg.add_legend()
+    plt.show()
+    return
+
+
+def split_data(data_frame, split_ratio):
+    label = data_frame[['label']]
+    data_frame.drop(['label'], axis=1, inplace=True)
+    train_data, val_data, train_label, val_label = train_test_split(data_frame,
+                                                                    label,
+                                                                    test_size=split_ratio,
+                                                                    random_state=2020)
+    return train_data, val_data, train_label, val_label
+
+
+def get_time_year(data_frame, column_name):
+    data_frame[column_name] = data_frame[column_name].apply(lambda x: x if len(x) > 10 else (x + " 00:00:00"))
+    data_frame[column_name] = data_frame[column_name].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+    year = data_frame[column_name].apply(lambda x: x.year)
+    return year
+
+
+def probed_data(data_frame, model):
+    label = data_frame['label']
+    data_frame.drop(['label'], inplace=True, axis=1)
+    prob = pd.Series(model.predict_proba(data_frame)[:, 1], name='prob')
+    data_frame = pd.concat([data_frame.reset_index(),label.reset_index(),prob.reset_index()],axis=1)
+    return data_frame
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
+
+
+
